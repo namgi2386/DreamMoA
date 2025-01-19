8a0ffe57d96dcf83107c64adac5d149dadba0904
cod0216 <cod0216@nate.com>
2025-01-19 15:09:25 +0900

    init : 프로젝트 세팅

---
+++ src/main/java/com/garret/dreammoa/DreammoaApplication.java
@@ -0,0 +1,13 @@
+package com.garret.dreammoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreammoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreammoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garret/dreammoa/DreammoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garret.dreammoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreammoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
