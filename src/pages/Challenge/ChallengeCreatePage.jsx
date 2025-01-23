8789bce66a94cc6b12e8019e775e88f978586c7b
권남희 <knaem.edu@gmail.com>
2025-01-23 13:26:29 +0900

    Faet: 라우트 설정

---
+++ src/pages/Challenge/ChallengeCreatePage.jsx
@@ -0,0 +1,5 @@
+export default function ChallengeCreatePage() {
+  return (
+    <div>ChallengeCreatePage</div>
+  );
+}
--- src/pages/User/LoginPage.jsx
+++ src/pages/User/LoginPage.jsx
@@ -1,20 +1,18 @@
-import LoginForm from '../components/auth/LoginForm.jsx';
-import SocialLoginButton from '../components/common/SocialLoginButton.jsx';
-import { socialLogin } from '../services/api/authApi.js';
+import LoginForm from "../../components/auth/LoginForm.jsx";
+import SocialLoginButton from "../../components/common/SocialLoginButton.jsx";
+import { socialLogin } from "../../services/api/authApi.js";
 
 const LoginPage = () => {
   const handleGoogleLogin = () => {
     console.log("구글 로그인!");
-    socialLogin('google')
-  }
+    socialLogin("google");
+  };
   const handleNaverLogin = () => {
     console.log("네이버버 로그인!");
-    
-  }
+  };
   const handleKakaoLogin = () => {
     console.log("카카오오 로그인!");
-    
-  }
+  };
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-3 bg-white p-8 rounded-lg shadow-lg">
@@ -27,12 +25,12 @@ const LoginPage = () => {
           </p>
         </div>
         <LoginForm />
-        <p className='text-xs text-right'>forgot your password?</p>
-        <p className='text-xs'>Or continue with</p>
+        <p className="text-xs text-right">forgot your password?</p>
+        <p className="text-xs">Or continue with</p>
         <div className="space-y-3">
-        <SocialLoginButton provider="google" onClick={handleGoogleLogin}/>
-        <SocialLoginButton provider="naver" onClick={handleNaverLogin}/>
-        <SocialLoginButton provider="kakao" onClick={handleKakaoLogin}/>
+          <SocialLoginButton provider="google" onClick={handleGoogleLogin} />
+          <SocialLoginButton provider="naver" onClick={handleNaverLogin} />
+          <SocialLoginButton provider="kakao" onClick={handleKakaoLogin} />
         </div>
       </div>
     </div>
