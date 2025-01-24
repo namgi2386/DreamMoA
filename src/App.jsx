e81e2cf7dde16dfe5b518df2ec5a17da498f4c27
권남희 <knaem.edu@gmail.com>
2025-01-24 17:29:34 +0900

    Feat: 푸터 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
-import { RecoilRoot } from 'recoil';
-import { Suspense } from 'react';
-import AppRoutes from './routes';
-import Loading from './components/common/Loading';
-import Header from './components/layout/Header';
+import "./App.css";
+import { RecoilRoot } from "recoil";
+import { Suspense } from "react";
+import AppRoutes from "./routes";
+import Loading from "./components/common/Loading";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 // import TestLayoutPage from './pages/TestLayoutPage';
 
 export default function App() {
@@ -12,10 +13,11 @@ export default function App() {
       <div className="min-h-screen bg-gray-300 dark:bg-gray-800">
         <div className="mx-auto min-h-screen bg-white dark:bg-gray-900 dark:text-gray-100">
           <Header />
-          <Suspense fallback={<Loading/>}>
+          <Suspense fallback={<Loading />}>
             {/* <TestLayoutPage/> */}
             <AppRoutes />
           </Suspense>
+          <Footer />
         </div>
       </div>
     </RecoilRoot>
