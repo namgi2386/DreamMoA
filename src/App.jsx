c26787dab1ff29098746e0bc36015520a04e85d3
권남희 <knaem.edu@gmail.com>
2025-01-24 17:38:10 +0900

    Feat: 로그인, 회원가입 페이지에서 푸터 렌더링 안함

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { RecoilRoot } from "recoil";
 import { Suspense } from "react";
+import { useLocation } from "react-router-dom";
 import AppRoutes from "./routes";
 import Loading from "./components/common/Loading";
 import Header from "./components/layout/Header";
@@ -8,17 +9,22 @@ import Footer from "./components/layout/Footer";
 // import TestLayoutPage from './pages/TestLayoutPage';
 
 export default function App() {
+  const location = useLocation();
+  const hideFooterPaths = ["/join", "/login"];
+
+  const shouldHideFooter = hideFooterPaths.includes(location.pathname);
+
   return (
     <RecoilRoot>
       <div className="min-h-screen bg-gray-300 dark:bg-gray-800">
-        <div className="mx-auto min-h-screen bg-white dark:bg-gray-900 dark:text-gray-100">
-          <Header />
+        <Header />
+        <main className="flex-grow">
           <Suspense fallback={<Loading />}>
             {/* <TestLayoutPage/> */}
             <AppRoutes />
           </Suspense>
-          <Footer />
-        </div>
+        </main>
+        {!shouldHideFooter && <Footer />} {/* 조건부로 Footer 렌더링 */}
       </div>
     </RecoilRoot>
   );
