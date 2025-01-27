f25ff33695ca382accfa35ebc537bb834ae419fe
cod0216 <cod0216@nate.com>
2025-01-27 15:01:58 +0900

    fix: restore User

--- src/main/java/com/garret/dreammoa/domain/dto/user/request/JoinRequest.java
+++ src/main/java/com/garret/dreammoa/domain/dto/user/request/JoinRequest.java
@@ -35,6 +35,6 @@ public class JoinRequest {
     )
     private String nickname;
 
-//    @NotEmpty(message = "인증 코드는 필수입니다.")
-//    private String code;
+    @AssertTrue(message = "이메일 인증이 필요합니다.")
+    private boolean verifyEmail;
 }
