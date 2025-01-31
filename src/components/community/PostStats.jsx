bd650c6c1efd1e3eb939be85083980ee1a7ea577
김소연 <soyeon3589@naver.com>
2025-01-31 14:27:00 +0900

    FE: 소연 좋아요기능 1차 커밋

--- src/components/community/PostStats.jsx
+++ src/components/community/PostStats.jsx
@@ -1,16 +1,18 @@
 // 좋아요 & 댓글 개수를 표시
+import { FaRegComment } from "react-icons/fa";
+import LikeToggleButton from "./LikeToggleButton";
 
-import { FaRegThumbsUp, FaRegComment } from "react-icons/fa";
+export default function PostStats({ postId, likes = 0, comments = 0 }) {
+  return (
+    <div className="flex items-center space-x-6 mt-6 text-gray-500">
+      {/* 좋아요 부분을 LikeToggleButton으로 대체 */}
+      <LikeToggleButton postId={postId} initialLikes={likes} />
 
-export default function PostStats({ likes, comments }) {
-    return (
-      <div className="flex items-center space-x-6 mt-6 text-gray-500">
-        <span className="flex items-center gap-1">
-          <FaRegThumbsUp /> {likes || 0} 좋아요
-        </span>
-        <span className="flex items-center gap-1">
-          <FaRegComment /> {comments || 0} 댓글
-        </span>
-      </div>
-    );
-  }
+      {/* 댓글 부분은 그대로 유지 */}
+      <span className="flex items-center gap-1">
+        <FaRegComment />
+        {comments || 0} 댓글
+      </span>
+    </div>
+  );
+}
