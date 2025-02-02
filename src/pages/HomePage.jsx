d3fd96cd88108141d2d82fe82bd60b2d2a706938
임남기 <happynj2697@gmail.com>
2025-02-03 08:53:29 +0900

    Test: 홈페이지 toplinesection v1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,17 @@ import ServiceHighlight from '../components/home/ServiceHighlight';
 import AIFeatureSection from '../components/home/AIFeatureSection';
 import { useSocialLogin } from '../hooks/useSocialLogin';
 import HomeCommunity from '../components/home/homeCommunitySection/HomeCommunity';
-import ReviewGradation from '../components/home/ReviewSection/ReviewGradation';
+import TopLine from '../components/home/topLineSection/TopLine';
 
 export default function HomePage() {
   useSocialLogin();
   return (
     <div className="min-h-screen w-full">
+      <TopLine/>
       <HomeCommunity/>
       <MainHero />
       <ServiceHighlight />
       <AIFeatureSection />
-      <ReviewGradation/>
     </div>
   );
 }
