79c80a212809f93166197a2500ef0ebfc1110307
김한주 <ksb2725@naver.com>
2025-02-03 22:20:16 +0900

    Update FE/src/services/api/axios.js

--- FE/src/services/api/axios.js
+++ FE/src/services/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 인스턴스 생성
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://dreammoa.duckdns.org:8080",
   withCredentials: true, 
   headers: {
     Accept: "application/json",
