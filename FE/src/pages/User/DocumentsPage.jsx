3fa68c2ce23e1f9db6f36308d1f2d59ae27097fa
임남기 <happynj2697@gmail.com>
2025-02-07 00:17:16 +0900

    Feat: 태그 공통모달

--- FE/src/pages/User/DocumentsPage.jsx
+++ FE/src/pages/User/DocumentsPage.jsx
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 import testlogo from "../../assets/logo/testlogo.png";
 import CallThePoliceModal from "../../components/common/modal/CallThePoliceModal";
 import { useState } from "react";
+import { useRecoilValue } from 'recoil';
+import { selectedTagsState } from '/src/recoil/atoms/tags/selectedTagsState';
+import TagSelector from "../../components/common/tags/TagSelector";
 
 export default function DocumentsPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const selectedTags = useRecoilValue(selectedTagsState);
   return (
     <>
       <div className="fixed top-0 left-0 right-0 z-10 p-4 flex justify-start">
@@ -32,7 +36,20 @@ export default function DocumentsPage() {
         <button onClick={() => setIsModalOpen(true)}>
           신고하기테스트버튼
         </button>
-
+        <div>
+          <h2>선택된 태그:</h2>
+          <div className="flex gap-2">
+            {selectedTags.map((tag) => (
+              <span 
+                key={tag} 
+                className="px-2 py-1 bg-blue-500 text-white rounded-full text-sm"
+              >
+                #{tag}
+              </span>
+            ))}
+          </div>
+        </div>
+        <TagSelector/>
 
       </div>
     </>
