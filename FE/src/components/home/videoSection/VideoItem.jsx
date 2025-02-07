20bc6b10622816a5b88c404685c3540c1cbaa171
임남기 <happynj2697@gmail.com>
2025-02-08 02:53:02 +0900

    Chore: 주석 추가

--- FE/src/components/home/videoSection/VideoItem.jsx
+++ FE/src/components/home/videoSection/VideoItem.jsx
@@ -1,4 +1,7 @@
 export default function VideoItem({ title, content, videogif, bgcolor, location }) {
+  // w-1/2 : 너비의 절반사용 w-3/5 : 너비의 60%사용용
+  // location === 'left' ? 'md:rounded-l-2xl' : 'md:rounded-r-2xl'
+  // 왼쪽이라면 왼쪽에 라운드주기 단, md보다 작아지면 라운드제거거
   const videoSection = (
     <div className="w-1/2 h-full overflow-hidden transition-all duration-800 hover:w-3/5">
       <img 
@@ -23,6 +26,7 @@ export default function VideoItem({ title, content, videogif, bgcolor, location 
     <div className="flex flex-col md:flex-row items-center w-full max-w-7xl mx-auto 
                     p-4 h-80 transition-all duration-300 hover:scale-105 
                     cursor-default select-none">
+      {/* location값에따라 영상위치 변경 */}
       {location === 'left' ? (
         <>
           {videoSection}
