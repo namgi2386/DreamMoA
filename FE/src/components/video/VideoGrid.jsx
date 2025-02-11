3766d1d0238358e7f1ffc73291cd2df4908c5c8a
권남희 <knaem.edu@gmail.com>
2025-02-11 11:31:14 +0900

    Feat: 전체 비디오 레이아웃 구현 - 반응형: 모바일, 데스크탑 - 메인 스트림과 참가자 목록을 구분 - 클릭 시 메인화면 전환

--- FE/src/components/video/VideoGrid.jsx
+++ FE/src/components/video/VideoGrid.jsx
@@ -1,42 +1,49 @@
-import UserVideoComponent from './UserVideo';
+// import UserVideoComponent from './UserVideo';
+import UserVideo from "./UserVideo";
 
 // mainStreamManager : 메인 스트림
 // publisher         : 자신의 스트림
 // subscribers       : 다른 참가자들의 스트림
 // onStreamClick     : 스트림 클릭 핸들러
-export default function VideoGrid({mainStreamManager, publisher, subscribers, onStreamClick}) {
+
+const VideoGrid = ({
+  mainStreamManager, // 메인 화면에 표시될 스트림
+  publisher, // 자신의 비디오 스트림
+  subscribers, // 다른 참가자들의 스트림 배열
+  onStreamClick, // 스트림 클릭 시 메인 화면 전환 핸들러
+}) => {
   return (
-    <div className="grid grid-cols-3 gap-4 h-[calc(100%-80px)]">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-full p-4">
       {/* 메인 스트림 영역 */}
-      {mainStreamManager && (
-        <div className="col-span-2 bg-gray-800 rounded">
-          <UserVideoComponent streamManager={mainStreamManager} />
-        </div>
-      )}
-      
+      <div className="md:col-span-2 bg-gray-800 rounded-lg overflow-hidden h-[calc(100vh-200px)]">
+        {mainStreamManager && <UserVideo streamManager={mainStreamManager} />}
+      </div>
+
       {/* 참가자 목록 영역 */}
-      <div className="space-y-4">
+      <div className="space-y-4 overflow-y-auto h-[calc(100vh-200px)]">
         {/* 자신의 스트림 */}
         {publisher && (
           <div
             onClick={() => onStreamClick(publisher)}
-            className="bg-gray-800 rounded cursor-pointer"
+            className="h-48 bg-gray-800 rounded-lg overflow-hidden cursor-pointer hover:ring-2 hover:ring-blue-500 transition-all"
           >
-            <UserVideoComponent streamManager={publisher} />
+            <UserVideo streamManager={publisher} />
           </div>
         )}
-        
+
         {/* 다른 참가자들의 스트림 */}
-        {subscribers.map((sub) => (
+        {subscribers.map((subscriber) => (
           <div
-            key={sub.stream.connection.connectionId}
-            onClick={() => onStreamClick(sub)}
-            className="bg-gray-800 rounded cursor-pointer"
+            key={subscriber.stream.connection.connectionId}
+            onClick={() => onStreamClick(subscriber)}
+            className="h-48 bg-gray-800 rounded-lg overflow-hidden cursor-pointer hover:ring-2 hover:ring-blue-500 transition-all"
           >
-            <UserVideoComponent streamManager={sub} />
+            <UserVideo streamManager={subscriber} />
           </div>
         ))}
       </div>
     </div>
   );
 };
+
+export default VideoGrid;
