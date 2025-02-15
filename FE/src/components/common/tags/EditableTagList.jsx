74075fb9e31ce8b727f78e10843c8fac0216566c
권남희 <knaem.edu@gmail.com>
2025-02-16 01:26:49 +0900

    Style: placeholder css 통일

--- FE/src/components/common/tags/EditableTagList.jsx
+++ FE/src/components/common/tags/EditableTagList.jsx
@@ -67,9 +67,15 @@ export default function EditableTagList({
     // 태그가 없고 edit 모드가 아닐 때
     if (!isEdittag && selectedTags.length === 0) {
       return (
-        <motion.p variants={tagVariants} className="text-gray-500 text-lg">
-          관심있는 태그를 설정해보세요.
-        </motion.p>
+        <div className="flex justify-center items-center h-full">
+          <motion.p
+            variants={tagVariants}
+            // 와 이거 엄청 좋다 폰트 특성에 따라서 여백이 이상해질 때 relative top-[?px] 이렇게 미세조정 가능능
+            className="text-gray-500 text-lg leading-none relative top-[10px]"
+          >
+            관심 있는 태그를 설정해보세요
+          </motion.p>
+        </div>
       );
     }
 
