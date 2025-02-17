75effb21445521e843e34d1b55e00634885630f5
김소연 <soyeon3589@naver.com>
2025-02-17 14:59:01 +0900

    Feat: FE 소연 대시보드 2차 커밋

--- FE/src/components/dashboard/common/DataLabel.jsx
+++ FE/src/components/dashboard/common/DataLabel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DataLabel({ label }) {
   return (
-    <div className="px-4 py-2 border border-gray-400 rounded-md bg-gray-100 text-gray-800 text-center font-bold">
+    <div className="px-4 py-2 rounded-md bg-yellow-100 text-hmy-blue-1 text-center font-bold">
       {label}
     </div>
   );
